50eb06afb3bbb1c92679d4cd00cd761d31ac4cac
eunsdaily <mount_e@naver.com>
2017-03-07 10:12:00 +0900

    please.. help me

---
+++ sosi_git_test/src/sosi_git_test/Eun.java
@@ -0,0 +1,10 @@
+package sosi_git_test;
+
+public class Eun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("201424406 KWON EUN");
+	}
+
+}
