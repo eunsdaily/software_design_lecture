82b2948a270de1edfe64922b505762c3599f3848
eunsdaily <mount_e@naver.com>
2017-03-07 10:34:22 +0900

    force 안하고 되나?

--- sosi_git_test/src/sosi_git_test/Eun.java
+++ sosi_git_test/src/sosi_git_test/Eun.java
@@ -5,7 +5,7 @@ public class Eun {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("201424406 KWON EUN");
-		System.out.println("Success!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("Success!!!!!!!!!!!!!!!!");
 	}
 
 }
