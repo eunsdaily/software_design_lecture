03b3e0938e943fa5a2c85157488c44838a418de3
cse <cse@IP-159>
2017-03-14 09:59:51 +0900

    change test file

--- JunitTest/src/ScoreReportTest.java
+++ JunitTest/src/ScoreReportTest.java
@@ -5,7 +5,22 @@ import org.junit.Test;
 public class ScoreReportTest {
 
 	@Test
-	public void testAddTwoScore() {
+	public void testScoreReport() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetScoreList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddScore() {
 		ScoreReport scoreReport = new ScoreReport();
 		for (int i=0; i<20; i++)
 			scoreReport.addScore(10);
@@ -13,11 +28,87 @@ public class ScoreReportTest {
 	}
 
 	@Test
-	public void testAllScoreAverage() {
+	public void testClearScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAverage() {
 		ScoreReport scoreReport = new ScoreReport();
 		for (int i=0; i<20; i++)
 			scoreReport.addScore(10);
 		
 		assertEquals(10, scoreReport.getAverage());
 	}
+
+	@Test
+	public void testGetReport() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
 }
